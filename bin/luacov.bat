3f8fc242019cd7587a953482283d7cf2b1749474
Hisham Muhammad <hisham@gobolinux.org>
2012-10-09 17:15:28 -0300

    Move wrapper scripts to a separate directory, rename main script back to luacov

--- bin/luacov.bat
+++ bin/luacov.bat
@@ -11,5 +11,5 @@ if defined FOUND (
 if "%cmd%"=="" (
   echo "LuaCov requires that a valid execution environment be specified (or that you have lua or luajit accessible in your PATH). Aborting."
 ) else (
-  call "%~dp0%cmd%" "%~dp0luacov_bootstrap" %*
+  call "%~dp0%cmd%" "%~dp0luacov" %*
 )
--- bin/luadoc.bat
+++ bin/luadoc.bat
@@ -1,3 +1,4 @@
+cd ..
 call luadoc_start -r README.md -d doc\doc\ .\src
 call doc\doc\index.html
 pause

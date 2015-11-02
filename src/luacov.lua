ed9e2848d2c5ba0923e9a0994d830c51e06f79ad
mpeterv <mpeterval@gmail.com>
2015-11-02 23:02:30 +0300

    Prepare to use ldoc to build reference

--- src/luacov.lua
+++ src/luacov.lua
@@ -2,7 +2,7 @@
 -- Useful for launching scripts from the command-line. Returns the <code>luacov.runner</code> module.
 -- @class module
 -- @name luacov
--- @example  lua -lluacov sometest.lua 
+-- @usage lua -lluacov sometest.lua
 local runner = require("luacov.runner")
 runner.init()
 return runner

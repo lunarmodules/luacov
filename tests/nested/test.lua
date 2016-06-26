8b4cbc972fdab88e2af84022b2a54c10dcd17b41
Peter Melnichenko <mpeterval@gmail.com>
2016-06-26 15:23:57 +0300

    Fix cli/nested test

--- tests/nested/test.lua
+++ tests/nested/test.lua
@@ -4,14 +4,14 @@ local luacov = require "luacov.runner"
 testlib.f1()
 
 local dir_sep = package.config:sub(1, 1)
-local cmd = arg[-5] or "lua"
+local cmd = arg[-4] or "lua"
 local slash = cmd:find(dir_sep)
 
 if slash and slash ~= 1 then
    cmd = ".." .. dir_sep .. cmd
 end
 
-cmd = ("%q"):format(cmd) .. ' -e "package.path=[[../?.lua;../../../src/?.lua;]]"'
+cmd = ("%q"):format(cmd) .. ' -e "package.path=[[../?.lua;../../../src/?.lua;]]..package.path"'
 cmd = cmd .. ' -e "osexit = os.exit"'
 cmd = cmd .. ' -e "require([[luacov.runner]]).load_config({statsfile = [[../luacov.stats.out]], savestepsize = 1})"'
 cmd = cmd .. " -l luacov.tick"

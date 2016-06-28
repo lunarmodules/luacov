c65e8084d2543b82c6bd2f5613b2fcb48a431511
Peter Melnichenko <mpeterval@gmail.com>
2016-06-28 23:53:32 +0300

    Add a test for `tick` option in config

--- tests/nested/test.lua
+++ tests/nested/test.lua
@@ -4,21 +4,28 @@ local luacov = require "luacov.runner"
 testlib.f1()
 
 local dir_sep = package.config:sub(1, 1)
-local cmd = arg[-4] or "lua"
-local slash = cmd:find(dir_sep)
+local lua = arg[-4] or "lua"
+local slash = lua:find(dir_sep)
 
 if slash and slash ~= 1 then
-   cmd = ".." .. dir_sep .. cmd
+   lua = ".." .. dir_sep .. lua
 end
 
-cmd = ("%q"):format(cmd) .. ' -e "package.path=[[../?.lua;../../../src/?.lua;]]..package.path"'
-cmd = cmd .. ' -e "osexit = os.exit"'
-cmd = cmd .. ' -e "require([[luacov.runner]]).load_config({statsfile = [[../luacov.stats.out]], savestepsize = 1})"'
-cmd = cmd .. " -l luacov.tick"
-cmd = cmd .. ' -e "dofile([[script.lua]])"'
-cmd = cmd:gsub("/", dir_sep)
+local function test(tick_as_module)
+   local config = tick_as_module and ".luacov" or "tick.luacov"
+   local mod = tick_as_module and "luacov.tick" or "luacov"
+   local cmd = ("%q"):format(lua) .. ' -e "package.path=[[../?.lua;../../../src/?.lua;]]..package.path"'
+   cmd = cmd .. ' -e "osexit = os.exit"'
+   cmd = cmd .. ' -e "require([[luacov.runner]]).load_config([[' .. config .. ']])"'
+   cmd = cmd .. " -l " .. mod
+   cmd = cmd .. ' -e "dofile([[script.lua]])"'
+   cmd = cmd:gsub("/", dir_sep)
 
-local ok = os.execute("cd subdir && " .. cmd)
-assert(ok == 0 or ok == true)
+   local ok = os.execute("cd subdir && " .. cmd)
+   assert(ok == 0 or ok == true)
+end
+
+test(true)
+test(false)
 
 testlib.f2()

1e14db6d18596ebbc45f6462908252bb4f9700ff
wesen <wesen1@users.noreply.github.com>
2021-01-07 13:27:24 +0100

    Add includeuntestedfiles tests

---
+++ spec/includeuntestedfiles/example-src/moduleB/aLib.lua
@@ -0,0 +1,1 @@
+local val = "ue"
---
+++ spec/includeuntestedfiles/subdir/test.lua
@@ -0,0 +1,7 @@
+
+package.path = package.path .. ";../?.lua"
+
+-- Don't load some of the modules to prevent them from getting tested/executed
+require "example-src.moduleA.libA"
+require "example-src.third-module.lib-1"
+require "example-src.third-module.lib-2"

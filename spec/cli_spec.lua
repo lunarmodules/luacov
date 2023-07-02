dee3be256aba0a69a5ca62675ddf4b0044fb77ad
Matthew Brooks <mb964033@gmail.com>
2023-07-02 16:39:15 +0000

    See last, another attempt

--- spec/cli_spec.lua
+++ spec/cli_spec.lua
@@ -13,26 +13,14 @@ end
 local lua = get_lua()
 local dir_sep = package.config:sub(1, 1)
 
-local function check_status (status)
+local function exec(cmd)
+   local status = os.execute(cmd)
+
    if type(status) == "number" then
       assert.is_equal(0, status)
    else
       assert.is_true(status)
    end
-end
-
-local function exec(cmd, pre, post)
-
-  if pre then
-    check_status(os.execute(pre))
-  end
-
-  check_status(os.execute(cmd))
-
-  if post then
-    check_status(os.execute(post))
-  end
-
 end
 
 local function read_file(file)
@@ -48,12 +36,11 @@ end
 local function assert_cli(dir, enable_cluacov, expected_file, flags, configfp)
 
    local prefix = ""
-   local precmd = nil
-   local postcmd = nil
+   local postfix = ""
 
    if configfp and dir_sep == "\\" then
-     precmd = ("setx /m LUACOV_CONFIG %q"):format(configfp)
-     postcmd = "setx /m LUACOV_CONFIG \"\""
+     prefix = ("set \"LUACOV_CONFIG=%s\" &&"):format(configfp)
+     postfix = ("& set LUACOV_CONFIG=")
    elseif configfp then
      prefix = ("LUACOV_CONFIG=%q"):format(configfp)
    end
@@ -80,10 +67,10 @@ local function assert_cli(dir, enable_cluacov, expected_file, flags, configfp)
       init_lua = init_lua .. "; package.preload[ [[cluacov.version]] ] = error"
    end
 
-   exec(("cd %q && %s %q -e %q -lluacov test.lua %s"):format(test_dir, prefix, lua, init_lua, flags), precmd, postcmd)
+   exec(("cd %q && %s %q -e %q -lluacov test.lua %s %s"):format(test_dir, prefix, lua, init_lua, flags, postfix))
 
    local luacov_path = (src_path .. "/bin/luacov"):gsub("/", dir_sep)
-   exec(("cd %q && %s %q -e %q %s %s"):format(test_dir, prefix, lua, init_lua, luacov_path, flags), precmd, postcmd)
+   exec(("cd %q && %s %q -e %q %s %s %s"):format(test_dir, prefix, lua, init_lua, luacov_path, flags, postfix))
 
    expected_file = test_dir .. dir_sep .. expected_file
    local expected = read_file(expected_file)
